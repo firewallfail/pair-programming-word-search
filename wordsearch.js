788f440dacb483240c19fd242a0f713a25aebbc1
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-13 16:23:07 -0400

    Pair Programming with Brent - Requirements Implemented

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  let transposed = [];
+  let len = letters[0].length;
+
+  //transponse the array
+  for (let index = 0; index < len; index++) {
+    transposed[index] = letters.map(lett => lett[index]);
+  }
+  //convert to string
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transposed.map(ls => ls.join(''));
+  
+  //Get "word" line
+  for (let l of horizontalJoin) {
+  //looking for the word in the line
+    if (l.indexOf(word) > -1) return true;
+    if (l.split('').reverse().join('').indexOf(word) > -1) return true;
+  }
+  for (let j of verticalJoin) {
+    
+    //looking for the word in the line
+    if (j.indexOf(word) > -1) return true;
+    if (j.split('').reverse().join('').indexOf(word) > -1) return true;
+  }
+  return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
